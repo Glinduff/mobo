4dec386054b392ac16856e807062aabb32dcb18b
Nicolas <nh@beenary.cl>
2018-08-03 16:00:20 -0400

    added: react maps

--- src/assignment/Assignment.js
+++ src/assignment/Assignment.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import OrderDateList from "../order/OrderDateList";
 import { getDateMD } from "../helpers/date";
+import GoogleMapReact from 'google-map-react';
 
 class Assignment extends Component {
 
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+
   render() {
     const {notAssigned} = this.props
-    return ( <OrderDateList dates={notAssigned} /> )
-  }j
+    return ( 
+      <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'row' }}>
+        <OrderDateList dates={notAssigned} />
+        <div style={{position: 'relative',  height: '100%', width: '100%'}}>
+          <div style={{ height: '100%', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'AIzaSyCGqjpXT0tEtJf6rHnvO0eXZrs4cb2L1_k'}}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}>
+            </GoogleMapReact>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps({order}){
